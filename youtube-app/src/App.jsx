a773698aae9ee9480aa322fbad18366dcf48ed18
KRISH826 <krishnendupanj@gmail.com>
2024-03-23 00:29:13 +0530

    all task are done

--- youtube-app/src/App.jsx
+++ youtube-app/src/App.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Feed from "./pages/Feed";
 import SearchResult from "./pages/SearchResult";
@@ -9,13 +9,20 @@ import VideoDetails from "./pages/VideoDetails";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const location = useLocation();
   return (
     <>
-      <div className='flex flex-col h-full'>
+      <div className='flex flex-col'>
         <Navbar />
         <div className='flex flex-row h-full'>
-          <Sidebar />
-          <main className='py-2 px-5 flex-1'>
+          {(location.pathname === "/" || "/searchresult/searchQuery") && (
+            <Sidebar />
+          )}
+          <main
+            className={`py-2 px-5 flex-1 ${
+              (location.pathname === "/" || "/searchresult/searchQuery") &&
+              "pl-[250px]"
+            }`}>
             <Routes>
               <Route path='/' element={<Feed />} />
               <Route
--- youtube-app/src/pages/SearchResult.jsx
+++ youtube-app/src/pages/SearchResult.jsx
@@ -1,9 +1,14 @@
 /** @format */
 
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../context/contextApi";
+import { useParams } from "react-router-dom";
 
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  const { loading, setResult, result } = useContext(Context);
+  const { searchQuery } = useParams();
+  const [datazResult, setdataResult] = useState([]);
+  return <div className='my-auto mx-auto w-[1080px]'>SearchResult</div>;
 };
 
 export default SearchResult;
